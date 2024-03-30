e2f247c7bd88ddfa071c0206f8b3e4d3f894326e
lucaslomba <lucasazevedolomba@gmail.com>
2024-03-30 14:39:18 -0300

    Removendo rotas que nao serao usadas

--- app.py
+++ app.py
@@ -2,13 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello world!"
-
-@app.route("/about")
-def about():    
-    return "Sobre"
-
 if __name__ == "__main__":
     app.run(debug=True)
