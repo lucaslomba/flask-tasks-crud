cf8882b96a802bb8faae1dc6dea48917dae5b9e8
lucaslomba <lucasazevedolomba@gmail.com>
2024-03-30 14:51:23 -0300

    Add route and class of tasks

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+from models.task import Task
 
 app = Flask(__name__)
+
+tasks = []
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    print(data)
+    return 'Test'
 
 if __name__ == "__main__":
     app.run(debug=True)
