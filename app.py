c923d49edb3715084efd5eced47dd53aa6366d1d
lucaslomba <lucasazevedolomba@gmail.com>
2024-03-31 00:22:57 -0300

    Add route to search for task id

--- app.py
+++ app.py
@@ -21,9 +21,17 @@ def get_tasks():
     task_list = [task.to_dict() for task in tasks]
     output = {
         "tasks": task_list,
-        "total_tasks": 0
+        "total_tasks": len(task_list)
     }
     return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+        
+    return jsonify({"messsage": "Não foi possível encontrar a atividade"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
