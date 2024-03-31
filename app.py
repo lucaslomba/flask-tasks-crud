5e74d6f4fbf1e628692e1958d86cd81c40d2a1ee
lucaslomba <lucasazevedolomba@gmail.com>
2024-03-31 00:16:00 -0300

    Add get route to read tasks

--- app.py
+++ app.py
@@ -16,5 +16,14 @@ def create_task():
     print(tasks)
     return jsonify({"message": "Nova tarefa criada com successo"})
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    output = {
+        "tasks": task_list,
+        "total_tasks": 0
+    }
+    return jsonify(output)
+
 if __name__ == "__main__":
     app.run(debug=True)
