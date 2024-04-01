28ffba415052091eed064875f32616618c88706b
lucaslomba <lucasazevedolomba@gmail.com>
2024-04-01 20:32:38 -0300

    Route to update data

--- app.py
+++ app.py
@@ -33,5 +33,22 @@ def get_task(id):
         
     return jsonify({"messsage": "Não foi possível encontrar a atividade"}), 404
 
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+
+    if task == None:
+        return jsonify({"messsage": "Não foi possível encontrar a atividade"}), 404
+
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+
+    return jsonify({"messsage": "Tarefa atualizada com sucesso!"})
+
 if __name__ == "__main__":
     app.run(debug=True)
