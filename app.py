5c1528eb51f03f12fbdd87175cad46e3e8156b70
lucaslomba <lucasazevedolomba@gmail.com>
2024-04-03 08:16:02 -0300

    Using Id return to test route

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ def create_task():
     new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
     task_id_control += 1
     tasks.append(new_task)
-    print(tasks)
-    return jsonify({"message": "Nova tarefa criada com successo"})
+    return jsonify({"message": "Nova tarefa criada com successo", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
--- tests.py
+++ tests.py
@@ -13,3 +13,7 @@ def test_create_task():
 
     response = requests.post(f"{BASE_URL}/tasks", json=new_task_data)
     assert response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+    assert "id" in response_json
+    tasks.append(response_json['id'])
