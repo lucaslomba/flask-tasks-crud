810ca4fac0f275676f7c2446ac5a7d22429c1bd5
lucaslomba <lucasazevedolomba@gmail.com>
2024-04-03 08:25:03 -0300

    Apply test in get routes

--- tests.py
+++ tests.py
@@ -10,10 +10,24 @@ def test_create_task():
         "title": "Nova tarefa",
         "description": "Descricao da nova tarefa"
     }
-
     response = requests.post(f"{BASE_URL}/tasks", json=new_task_data)
     assert response.status_code == 200
     response_json = response.json()
     assert "message" in response_json
     assert "id" in response_json
     tasks.append(response_json['id'])
+
+def test_get_tasks():
+    response = requests.get(f"{BASE_URL}/tasks")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "tasks" in response_json
+    assert "total_tasks" in response_json
+
+def test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert task_id == response_json['id']
